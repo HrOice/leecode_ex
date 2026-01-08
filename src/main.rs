0a25c77252c451141e68ff0663896bf5b46adcf7
bowen <luanbowen@aihuicai.cn>
2026-01-08 20:09:45 +0800

    string stash

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod array;
 mod link;
 mod hash;
+mod string;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/string/lc_344.rs
@@ -0,0 +1,37 @@
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+//
+//
+//
+// 示例 1：
+//
+// 输入：s = ["h","e","l","l","o"]
+// 输出：["o","l","l","e","h"]
+// 示例 2：
+//
+// 输入：s = ["H","a","n","n","a","h"]
+// 输出：["h","a","n","n","a","H"]
+//
+
+pub fn reverse_string(s: &mut Vec<char>) {
+    let mut left = 0;
+    let mut right = s.len() - 1;
+    while left < right {
+        let tmp = s[right];
+        s[right] = s[left];
+        s[left] = tmp;
+        left += 1;
+        right -= 1;
+    }
+}
+
+#[test]
+fn test_reverse_string() {
+    let mut s = vec!['h', 'e', 'l', 'l', 'o'];
+    reverse_string(&mut s);
+    println!("{:?}", s);
+    let mut s = vec!['H','a','n','n','a','h'];
+    reverse_string(&mut s);
+    println!("{:?}", s);
+}
